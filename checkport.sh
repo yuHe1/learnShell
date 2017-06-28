87af618872f91b8e45d309b206e173074f23bc22
yuHe1 <yu.h.he@oracle.com>
2017-06-28 19:06:56 +0800

    Test empty string

--- checkport.sh
+++ checkport.sh
@@ -2,7 +2,10 @@
 port=80
 errors="$( netstat -aon | grep $port )"
 echo $errors
-if [ ${#errors[@]} -gt 0 ]; then
+# if [ ${#errors[@]} -gt 0 ]; then
+if [ !-z  errors ]; then
     echo "$port is used"
     exit 1
+else
+  echo "yes"
 fi
