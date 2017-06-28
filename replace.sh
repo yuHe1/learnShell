d765b24f4d7796fc4e94b29741ddade11dca8f58
yuHe1 <yu.h.he@oracle.com>
2017-06-28 05:29:15 -0700

    find and replace

---
+++ replace.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -ex
+old=$1
+new=$2
+file=$3
+
+for FIL in `find . -name $file -exec grep -l $old {} \;`
+       do
+                  echo 'Replacing in '$FIL
+                         perl -i -pe "s|$old|$new|g" $FIL
+                            done 
